286030e7560f53782c8c95462fcbee15bddfa023
Paul Peacock <pcp1976@gmail>
2018-12-29 22:54:45 +0000

    Need to install dev deps

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,4 +6,4 @@ APP_DIR = os.path.join(PROJECT_DIR, "app")
 VENV = os.path.join(APP_DIR, ".venv")
 os.mkdir(VENV)
 
-run("cd %s && pipenv run pipenv install" % APP_DIR, shell=True, check=True)
+run("cd %s && pipenv run pipenv install --dev" % APP_DIR, shell=True, check=True)
