2eebc84c8269b97d636c4b5c4c457ccb9b00ad44
Paul Peacock <pcp1976@gmail>
2018-12-29 14:20:27 +0000

    Add post install hook to generate virtualenv

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,9 @@
+from subprocess import run
+import os
+
+PROJECT_DIR = os.path.join(os.getcwd())
+APP_DIR = os.path.join(PROJECT_DIR, "app")
+VENV = os.path.join(APP_DIR, ".venv")
+os.mkdir(VENV)
+
+run("cd %s && pipenv run pipenv install" % APP_DIR), shell=True, check=True)
